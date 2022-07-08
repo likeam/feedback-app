0aef652f6c4d2df1a3b2557a137b4bfa479aba04
AbdulRehman <likeam99@gmail.com>
2022-07-09 00:44:22 +0500

    using state

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,7 +4,7 @@ import {useStae} from 'react'
 function FeedbackItem() {
     const rating = 7;
     const text = 'This is an example of Feedback item';
-   
+
     return (
         <div className="card">
             <div className="num-display">{rating}</div>
