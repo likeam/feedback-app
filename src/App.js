d3193bb1e3ff94b475df2c0103766185cbdbf55f
AbdulRehman <likeam99@gmail.com>
2022-07-12 15:40:35 +0500

    Event and popup building

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 
+
 function App(){
 
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -13,7 +14,8 @@ function App(){
         <Header/>
         <div className="container">
             <FeedbackList feedback = {feedback} />
-           
+          
+
         </div>
         </>
             )
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,26 @@
+import {FaTimes} from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Card from "./shared/Card"
 
-import {useStae} from 'react'
+function FeedbackItem({item}) {
 
-function FeedbackItem() {
-    const [rating, serRating] = useState(7)
-    const [text, setText] = useStae('This is an example of Feedback item')
+    const handleClick = (id) => {
+        console.log(id)
+    }
+        
 
-    return (
-        <div className="card">
-            <div className="num-display">{rating}</div>
-            <div className="text-display">{text}</div>
-            
-        </div>
-    )
+        return (
+            <Card>
+                <div className="num-display">{item.rating}</div>
+                <button onClick={() => handleClick(item.id)} className="close">
+                    <FaTimes color= 'purple'/>
+                </button>
+                <div className="text-display">{item.text}</div>
+                
+            </Card>
+        )
 }
-
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired,
+}
 export default FeedbackItem
